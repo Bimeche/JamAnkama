4190ff3c0639117485d927cbaa200bdebc8ca3fe
ANKAMA\pleduc <pleduc@ankama.com>
2017-12-15 22:29:37 +0100

    son vache

--- Assets/Scripts/CowManager.cs
+++ Assets/Scripts/CowManager.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class CowManager : AIManager {
 	public delegate void CowDiedAction (GameObject go);
+	public Transform fxHit;
 	public static event CowDiedAction OnCowDied;
 	private bool isCowVisible;
 	private Animator anim;
+	public AudioClip cowHit1;
+	public AudioClip cowHit2;
+	public AudioClip cowHit3;
+	public AudioClip Idle;
+	public AudioClip cowDeath1;
+	public AudioClip cowDeath2;
+	public AudioClip cowDeath3;
+	public AudioClip cowDeath4;
+	public AudioClip cowDeath5;
+	public AudioClip cowPanic1;
+	public AudioClip cowPanic2;
+	public AudioClip cowPanic3;
+	public AudioClip cowBounce1;
+	public AudioClip cowBounce2;
+	public AudioClip cowBounce3;
+	public AudioClip AttackHit1;
+	public AudioClip AttackHit2;
+	public AudioClip AttackHit3;
+	public AudioClip AttackHit4;
+	public AudioClip AttackHit5;
+	public AudioClip AttackHit6;
 
 	// Use this for initialization
 	void Start () {
@@ -27,32 +49,35 @@ public class CowManager : AIManager {
 	}
 
 	private void OnCollisionEnter2D (Collision2D collision) {
-		if(collision.gameObject.tag == "Player")
-		{
+		if (collision.gameObject.tag == "Player") {
 			anim.SetBool ("Striked", true);
-			if (collision.contacts.Length > 0)
-			{
-				Vector2 impactPoint = new Vector2(collision.contacts[0].point.x - transform.position.x, collision.contacts[0].point.y - transform.position.y);
-				Vector2 impactSpeed = new Vector2(collision.contacts[0].relativeVelocity.x - rb.velocity.x, collision.contacts[0].relativeVelocity.y - rb.velocity.y);
+			if (collision.contacts.Length > 0) {
+				Vector2 impactPoint = new Vector2 (collision.contacts [0].point.x - transform.position.x, collision.contacts [0].point.y - transform.position.y);
+				Vector2 impactSpeed = new Vector2 (collision.contacts [0].relativeVelocity.x - rb.velocity.x, collision.contacts [0].relativeVelocity.y - rb.velocity.y);
 			
 				impactPoint = -impactPoint.normalized;
-				float magnitude = Mathf.Sqrt(impactSpeed.magnitude) * playerForce;
+				Destroy (Instantiate (fxHit, collision.contacts [0].point, Quaternion.Euler (new Vector3 (0, 0, -1))).gameObject, 0.25f);
+				SoundManager.instance.RandomizeSfx (cowHit1, cowHit2, cowHit3);
+				SoundManager.instance.RandomizeSfx (AttackHit1, AttackHit2, AttackHit3, AttackHit4, AttackHit5, AttackHit6);
+
+				float magnitude = Mathf.Sqrt (impactSpeed.magnitude) * playerForce;
 				if (magnitude < minMagnitude)
 					magnitude = minMagnitude;
 
-				rb.AddForce(impactPoint * magnitude);
+				rb.AddForce (impactPoint * magnitude);
 			}
-		}
-		else if (collision.gameObject.tag == "Cow")
-		{
-			if(collision.contacts.Length > 0)
-			{
-				Vector2 impactPoint = new Vector2(collision.contacts[0].point.x - transform.position.x, collision.contacts[0].point.y - transform.position.y);
+		} else if (collision.gameObject.tag == "Cow") {
+			SoundManager.instance.RandomizeSfx (cowBounce1, cowBounce2, cowBounce3);
+			if (collision.contacts.Length > 0) {
+				Vector2 impactPoint = new Vector2 (collision.contacts [0].point.x - transform.position.x, collision.contacts [0].point.y - transform.position.y);
 
 				impactPoint = -impactPoint.normalized;
 
-				rb.AddForce(impactPoint * cowForce);
+				rb.AddForce (impactPoint * cowForce);
 			}
+		} 
+		else {
+			SoundManager.instance.RandomizeSfx (cowBounce1, cowBounce2, cowBounce3);
 		}
 	}
 
@@ -62,6 +87,7 @@ public class CowManager : AIManager {
 
 	private void OnBecameInvisible () {
 		if (isCowVisible && OnCowDied != null)
+			SoundManager.instance.RandomizeSfx (cowDeath1, cowDeath2, cowDeath3, cowDeath4, cowDeath5);
 			OnCowDied(gameObject);
 	}
 }
